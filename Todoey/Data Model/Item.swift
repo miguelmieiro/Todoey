f8902e0a79d57681e5df626b23d6998f3a543f90
Miguel Mieiro <miguelmieiro@miguels-macbook-pro.local>
2018-10-18 23:04:45 +0100

    Convert to MVC design patern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Miguel Mieiro on 18/10/2018.
+//  Copyright © 2018 Miguel Mieiro. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
